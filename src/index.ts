b069465b5f68df1e4939091d4823749c9ad08f80
sam new lap <sam@gmail.com>
2024-06-14 12:50:53 +0530

    any type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,9 @@ let sales: number = 12_34_56_789;
 let course: string = "TypeScript";
 let is_published: boolean = true;
 let level; //any
+level = 1;
+level = "test";
+
+function render(document) {
+  console.log(document);
+}
