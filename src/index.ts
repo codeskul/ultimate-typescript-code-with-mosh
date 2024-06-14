d55afbbd63795cf1cc012f92df6e7196be4c61cf
sam new lap <sam@gmail.com>
2024-06-14 12:24:50 +0530

    built in types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-let age: number = 10;
-if (age < 50) age += 10;
-console.log(age);
+let sales: number = 12_34_56_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level; //any
