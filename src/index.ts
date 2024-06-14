90de395ffedba2627ceea178a0a818c963da4d25
sam new lap <sam@gmail.com>
2024-06-14 13:29:01 +0530

    functions

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-const small = 1;
-const medium = 2;
-const large = 3;
-
-// Less optimized compiled javascript
-enum Size {
-  Small = 1,
-  Medium,
-  Large,
+// noUnusedParameters: true shows ERROR
+function calculateTax0(income: number): number {
+  return 0;
+}
+// function with return type number
+function calculateTax1(income: number): number {
+  return income * 1.2;
 }
 
-// More optimized compiled javascript
-// const enum Size {
-//   Small = 1,
-//   Medium,
-//   Large,
-// }
+// noImplicitReturns: true shows ERROR
+function calculateTax2(income: number) {
+  if (income < 50_000) return income * 1.2;
+  // undefined
+}
 
-let mySize: Size = Size.Medium;
-console.log(mySize);
+// noImplicitReturns: true ERROR solve
+function calculateTax3(income: number) {
+  if (income < 50_000) return income * 1.2;
+  return income * 1.3;
+}
+
+// noUnusedLocals: true shows ERROR
+function calculateTax4(income: number): number {
+  let x = 10;
+  if (income < 50_000) return income * 1.2;
+  return income * 1.3;
+}
+
+function calculateTax5(income: number, taxYear?: number): number {
+  if ((taxYear || 2022) < 2024) return income * 1.2;
+  return income * 1.3;
+}
+function calculateTax6(income: number, taxYear = 2022): number {
+  if (taxYear < 2024) return income * 1.2;
+  return income * 1.3;
+}
+
+calculateTax5(10_000, 2022);
