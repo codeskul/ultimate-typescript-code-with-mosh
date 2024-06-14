6b60c6a376281442588c50fa9e0d3453742716a7
sam new lap <sam@gmail.com>
2024-06-14 15:05:50 +0530

    objects

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,20 @@
-// noUnusedParameters: true shows ERROR
-function calculateTax0(income: number): number {
-  return 0;
-}
-// function with return type number
-function calculateTax1(income: number): number {
-  return income * 1.2;
-}
+let employee0: { id: number; name: string } = { id: 1, name: "Sanjay" };
 
-// noImplicitReturns: true shows ERROR
-function calculateTax2(income: number) {
-  if (income < 50_000) return income * 1.2;
-  // undefined
-}
+// readonly
+let employee1: { readonly id: number; name: string } = {
+  id: 1,
+  name: "Sanjay",
+};
 
-// noImplicitReturns: true ERROR solve
-function calculateTax3(income: number) {
-  if (income < 50_000) return income * 1.2;
-  return income * 1.3;
-}
-
-// noUnusedLocals: true shows ERROR
-function calculateTax4(income: number): number {
-  let x = 10;
-  if (income < 50_000) return income * 1.2;
-  return income * 1.3;
-}
-
-function calculateTax5(income: number, taxYear?: number): number {
-  if ((taxYear || 2022) < 2024) return income * 1.2;
-  return income * 1.3;
-}
-function calculateTax6(income: number, taxYear = 2022): number {
-  if (taxYear < 2024) return income * 1.2;
-  return income * 1.3;
-}
-
-calculateTax5(10_000, 2022);
+// function as object property
+let employee2: {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+} = {
+  id: 1,
+  name: "Sanjay",
+  retire: (date: Date) => {
+    console.log(date);
+  },
+};
